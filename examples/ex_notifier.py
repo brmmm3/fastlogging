e895281bfa72485625ef6a348c7b756dae7f82ca
Martin Bammer <mrbm74@gmail.com>
2020-02-08 11:51:26 +0100

    Update example ex_notifier.py

--- examples/ex_notifier.py
+++ examples/ex_notifier.py
@@ -11,7 +11,14 @@ if __name__ == "__main__":
     defaults["username"] = input("User:")
     defaults["password"] = input("PW:")
     hdlr = NotificationHandler('gmail', defaults=defaults)
-    hdlr.setLevel(logging.INFO)
-
     log.addHandler(hdlr)
-    log.info('And just like that, you get notified about all your errors!')
+    # Set log handler specific log level
+    hdlr.setLevel(logging.WARN)
+    # Set console specific log level (optional). If not set, the global log level will do the filtering.
+    log.setConsoleLevel(logging.WARN)
+    # Set global log level.
+    # Note: If global log level is higher than the log level of a log handler or the console then messages
+    # will be filtered nevertheless
+    log.setLevel(logging.INFO)
+    log.info('And just like that, you get notified about all your infos!')
+    log.warning('And just like that, you get notified about all your warnings!')
