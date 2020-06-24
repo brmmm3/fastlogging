7da09da7e8157b9e6cb6fc08b4870af52c96b22f
Marty B <mrbm74@gmail.com>
2020-06-24 07:38:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -138,6 +138,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     keywords='fast logging',
